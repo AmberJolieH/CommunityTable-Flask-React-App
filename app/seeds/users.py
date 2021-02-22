dcc43b885ebf389fca7f68697e5f19ba1610754b
Nicole Loescher <Nicole.Loescher91@gmail.com>
2021-02-22 12:17:30 -0700

    wrokgin on seeer for user

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -1,13 +1,34 @@
 from werkzeug.security import generate_password_hash
 from app.models import db, User
+import faker
+import bcrypt
 
 # Adds a demo user, you can add other users here if you want
 def seed_users():
-
-    demo = User(username='Demo', email='demo@aa.io',
-                password='password')
-
+    password = 'password'
+    
+    demo = User(
+        username='Demo', 
+        email='demo@aa.io',
+        phoneNumber=1235550123,
+        firstName='Demo',
+        lastName='User',
+        isOrg=False,
+        hashedPassword=bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt()))
     db.session.add(demo)
+    
+    times = 0
+    while times < 30:
+        newUser = User(
+            userName= faker.internet.userName(),
+            firstName= faker.name.firstName(),
+            lastName= faker.name.lastName(),
+            email= faker.internet.email(),
+            irOrg=False,
+            hashedPassword= bcrypt.hashpw(f'password{times}'.encode('utf8'), bcrypt.gensalt()),
+        ) 
+        times = times + 1
+        db.session.add(newUser)
 
     db.session.commit()
 
