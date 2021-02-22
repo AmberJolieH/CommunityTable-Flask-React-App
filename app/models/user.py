80ae9163b452361627301bd08b8356229130dcd4
Gabriel <gabriel.gutz@live.com>
2021-02-22 12:25:47 -0500

    Updated user model, created location model

--- app/models/user.py
+++ app/models/user.py
@@ -2,32 +2,43 @@ from .db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
+
 class User(db.Model, UserMixin):
-  __tablename__ = 'users'
+    __tablename__ = 'users'
 
-  id = db.Column(db.Integer, primary_key = True)
-  username = db.Column(db.String(40), nullable = False, unique = True)
-  email = db.Column(db.String(255), nullable = False, unique = True)
-  hashed_password = db.Column(db.String(255), nullable = False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(40), nullable=False, unique=True)
+    phonenumber = db.Column(db.Integer)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    firstname = db.Column(db.String(40), nullable=False)
+    lastname = db.Column(db.String(40), nullable=False)
+    isOrg = db.Column(db.Boolean, default=False)
+    hashed_password = db.Column(db.String(255), nullable=False)
+
+    @property
+    def password(self):
+        return self.hashed_password
+
+    @password.setter
+    def password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email
+        }
 
 
-  @property
-  def password(self):
-    return self.hashed_password
+class Location(db.Model):
+    __tablename__ = 'locations'
 
-
-  @password.setter
-  def password(self, password):
-    self.hashed_password = generate_password_hash(password)
-
-
-  def check_password(self, password):
-    return check_password_hash(self.password, password)
-
-
-  def to_dict(self):
-    return {
-      "id": self.id,
-      "username": self.username,
-      "email": self.email
-    }
+    id = db.Column(db.Integer, primary_key=True)
+    state = db.Column(db.String(30), nullable=False)
+    city = db.Column(db.String(30), nullable=False)
+    latitude = db.Column(db.Numeric(10, 8), nullable=False)
+    longitude = db.Column(db.Numeric(11, 8), nullable=False)
