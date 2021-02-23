fb0dde942c879accbaa7d75668df1edd7d17cfa6
Nicole Loescher <Nicole.Loescher91@gmail.com>
2021-02-22 19:51:27 -0700

    seeder for resources and locations complete

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,5 +1,8 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .resources import seed_resources, undo_resources
+from .locations import seed_locations, undo_locations
+# from .claimstatus import seed_claimStatus, undo_claimStatus
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -9,10 +12,16 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     seed_users()
+    seed_locations()
+    seed_resources()
+    # seed_claimStatus()
     # Add other seed functions here
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
     undo_users()
+    undo_locations()
+    undo_resources()
+    # undo_claimStatus()
     # Add other undo functions here
