6b7aa32b01ec4f27445784fd2b6cfe11891ebe98
Nicole Loescher <Nicole.Loescher91@gmail.com>
2021-02-25 14:13:23 -0700

    added resource detail card

--- react-app/src/components/Resources/ResourceDetail.js
+++ react-app/src/components/Resources/ResourceDetail.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { jsx } from "@emotion/react";
 import { getOneResource } from "../../store/resources"
 import { useParams } from "react-router-dom";
+import resourceImages from "./resourceImages"
 
 const ResourceDetail = () =>{
     const dispatch = useDispatch();
@@ -19,8 +20,29 @@ const ResourceDetail = () =>{
     if(!resource){
         return 'loading...'
     }
+
+    let imageContent;
+    const imageFinder = () =>{
+        if(!resource.image){
+            return imageContent = resourceImages[resource.catName]
+        }
+        else{
+            return imageContent = resource.image
+        }
+    }
     return(
-        <p>{resource.name + resource.id}</p>
+        <div className='standard-card'>
+            <h2>Name: {resource.name}</h2>
+
+            <img src={`${imageContent}`} />
+            <p>Description: {resource.description}</p>
+            <p>Category: {resource.catName}</p>
+            <p>Location: {resource.locationId}</p>
+            <p>Quantity: {resource.quantity}</p>
+            <p>Starts At: {resource.startsAt}</p>
+            <p>Ends At: {resource.endsAt}</p>
+            <p>Poster: {resource.posterId}</p>
+        </div>
     )
 }
 
---
+++ react-app/src/components/Resources/resourceImages.js
@@ -0,0 +1,15 @@
+const resourceImages = {
+    'Non-Perishable Food':'/static/images/non-perishable',
+    'Perishable Food': '/static/images/perishable',
+    'Water and beverages':'/static/images/beverages',
+    'Baby care': '/static/images/baby',
+    'Children toys': '/static/images/toys',
+    'Clothing': '/static/images/clothing',
+    'Electronics': '/static/images/electronics',
+    'Books': '/static/images/books',
+    'School Supplies': '/static/images/school',
+    'Furniture': '/static/images/furniture',
+    'Shelter': '/static/images/shelter',
+    'Services (Barber, shower, etc)': '/static/images/services',
+    'Other': '/static/images/other'
+}
