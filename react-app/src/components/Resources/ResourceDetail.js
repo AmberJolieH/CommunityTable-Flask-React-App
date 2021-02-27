005217e0a6ddb843f3f5bd870871dd7eed9dd38d
amberjolie <amberjolieh@gmail.com>
2021-02-27 17:17:01 -0600

    saturday

--- react-app/src/components/Resources/ResourceDetail.js
+++ react-app/src/components/Resources/ResourceDetail.js
@@ -29,15 +29,43 @@ const ResourceDetail = () =>{
     // else{
     //     imageContent = resource.image
     // }
-
-    
+    const cats = [
+        'Non-Perishable Food',
+        'Perishable Food',
+        'Water and beverages',
+        'Baby care',
+        'Children toys',
+        'Clothing',
+        'Electronics',
+        'Books',
+        'School Supplies',
+        'Furniture',
+        'Shelter',
+        'Services (Barber, shower, etc)',
+        'Other'
+    ]
+    const componentMap = {
+        'Non-Perishable Food': "https://resourceimage.s3-us-west-2.amazonaws.com/cans.svg",
+        'Perishable Food': "https://resourceimage.s3-us-west-2.amazonaws.com/parishable.svg",
+        'Water and beverages': "https://resourceimage.s3-us-west-2.amazonaws.com/WATER.svg",
+        'Baby care': "https://resourceimage.s3-us-west-2.amazonaws.com/diapers.svg",
+        'Children toys': "https://resourceimage.s3-us-west-2.amazonaws.com/CHILDS-toys.svg",
+        'Clothing': "https://resourceimage.s3-us-west-2.amazonaws.com/cloth.svg",
+        'Electronics': "https://resourceimage.s3-us-west-2.amazonaws.com/elec.svg",
+        'Books': "https://resourceimage.s3-us-west-2.amazonaws.com/books.svg",
+        'School Supplies': "https://resourceimage.s3-us-west-2.amazonaws.com/schoolSupplies.svg",
+        'Furniture': "https://resourceimage.s3-us-west-2.amazonaws.com/furn.svg",
+        'Shelter': "https://resourceimage.s3-us-west-2.amazonaws.com/shelter.svg",
+        'Services (Barber, shower, etc)': "https://resourceimage.s3-us-west-2.amazonaws.com/services.svg",
+        'Other': "https://resourceimage.s3-us-west-2.amazonaws.com/etc.svg",
+    };
     // console.log(imageContent)
 
     return(
         <div className='standard-card'>
             <h2>Name: {resource.name}</h2>
 
-            {/* <img css={{maxWidth: "100px"}} src={`${imageContent}`} alt={resource.catName}/> */}
+            <img css={{ maxWidth: "100px" }} src={componentMap[cat]}/>
             <p>Description: {resource.description}</p>
             <p>Category: {resource.catName}</p>
             <p>Location: {resource.location.name}</p>
