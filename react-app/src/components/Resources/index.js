700391a8df4c79e728c984b878d94aef0d5627de
amberjolie <amberjolieh@gmail.com>
2021-02-26 18:38:47 -0600

    all resources page complete

--- react-app/src/components/Resources/index.js
+++ react-app/src/components/Resources/index.js
@@ -1,21 +1,22 @@
 /** @jsx jsx */
 import React, { useEffect } from "react";
-import {useDispatch, useSelector} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { jsx } from "@emotion/react";
 import { listResources } from "../../store/resources";
 import resourceImages from "./resourceImages"
 import ResourceImage from "./resource-img-picker"
 import { Link } from "react-router-dom";
+import componentMap from "./resource-img-picker"
 
-const Resources = () =>{
+const Resources = () => {
     const dispatch = useDispatch()
     let resources = useSelector(state => state.resources.list.resources)
-    
-    useEffect(()=>{
+
+    useEffect(() => {
         dispatch(listResources())
-    },[dispatch])
-    
-    if(!resources){
+    }, [dispatch])
+
+    if (!resources) {
         return null;
     }
     const cats = [
@@ -34,49 +35,68 @@ const Resources = () =>{
         'Other'
     ]
 
+    const componentMap = {
+        'Non-Perishable Food': "https://resourceimage.s3-us-west-2.amazonaws.com/cans.svg",
+        'Perishable Food': "https://resourceimage.s3-us-west-2.amazonaws.com/parishable.svg",
+        'Water and beverages': "https://resourceimage.s3-us-west-2.amazonaws.com/WATER.svg",
+        'Baby care': "https://resourceimage.s3-us-west-2.amazonaws.com/diapers.svg",
+        'Children toys': "https://resourceimage.s3-us-west-2.amazonaws.com/CHILDS-toys.svg",
+        'Clothing': "https://resourceimage.s3-us-west-2.amazonaws.com/cloth.svg",
+        'Electronics': "https://resourceimage.s3-us-west-2.amazonaws.com/elec.svg",
+        'Books': "https://resourceimage.s3-us-west-2.amazonaws.com/books.svg",
+        'School Supplies': "https://resourceimage.s3-us-west-2.amazonaws.com/schoolSupplies.svg",
+        'Furniture': "https://resourceimage.s3-us-west-2.amazonaws.com/furn.svg",
+        'Shelter': "https://resourceimage.s3-us-west-2.amazonaws.com/shelter.svg",
+        'Services (Barber, shower, etc)': "https://resourceimage.s3-us-west-2.amazonaws.com/services.svg",
+        'Other': "https://resourceimage.s3-us-west-2.amazonaws.com/etc.svg",
+    };
 
     return (
         <div>
             <h1
-            css={{
-                color: 'rgb(149, 181, 60)',
-                textAlign: 'center'
-            }}> Resource Categories </h1>
+                css={{
+                    color: 'rgb(149, 181, 60)',
+                    textAlign: 'center'
+                }}> Resource Categories </h1>
             <div
-            css={{
-                display:'grid',
-                gridTemplateColumns: '18% 18% 18% 18% 18%',
-                padding: '2rem',
-                
-            }}>
-                {cats.map((cat, i)=> (
-                    <Link
-                    to={`/resources/categories/${i + 1}`}
-                    css={{
-                        display:'flex',
-                        flexDirection:'column',
-                        margin:'4rem',
-                        width: '100%',
-                        alignItems: 'center',
-                        fontSize: '1.2rem',
-                        color:'black',
-                        fontFamily: 'monospace',
-                        textDecoration: 'none',
-                        textAlign:'center'
-                    }}>
+                css={{
+                    display: 'grid',
+                    gridTemplateColumns: '18% 18% 18% 18% 18%',
+                    padding: '2rem',
 
-                        <ResourceImage/>
-                            <img src={resourceImages[cat]} alt={cat} css={{
-                                maxWidth:'90%',
-                                borderRadius:'100%',
-                                margin: '1rem'
-                            }}/>
-                            <p key={cat} >
-                                {cat}
-                            </p>
-                        </Link>
-                    )
-                    )}
+                }}>
+                {cats.map((cat, i) => (
+                    <Link
+                        to={`/resources/categories/${i + 1}`}
+                        css={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            margin: '4rem',
+                            width: '100%',
+                            alignItems: 'center',
+                            fontSize: '1.2rem',
+                            color: 'black',
+                            fontFamily: 'monospace',
+                            textDecoration: 'none',
+                            textAlign: 'center'
+                        }}>
+
+                        {/* <ResourceImage
+                            name={"Books"}/>
+                        <p key={cat} >
+                            {cat}
+                        </p>  */}
+                        <img src={componentMap[cat]} alt={cat} css={{
+                            maxWidth: '90%',
+                            borderRadius: '100%',
+                            margin: '1rem'
+                        }} />
+                        <p key={cat} >
+                            {cat}
+                        </p>
+                    </Link>
+                )
+                )}
             </div>
         </div>
     )
--- react-app/src/components/Resources/resource-img-picker.js
+++ react-app/src/components/Resources/resource-img-picker.js
@@ -6,13 +6,14 @@ import { ReactComponent as baby } from "../../images/diapers.svg";
 import { ReactComponent as childCare } from "../../images/CHILDS-toys.svg";
 import { ReactComponent as cloth } from "../../images/cloth.svg";
 import { ReactComponent as elec } from "../../images/elec.svg";
-import { ReactComponent as book } from "../../images/books.svg";
+// import { ReactComponent as book } from "../../images/books.svg";
 import { ReactComponent as school } from "../../images/schoolSupplies.svg";
 import { ReactComponent as furn } from "../../images/furn.svg";
 import { ReactComponent as shelt } from "../../images/shelter.svg";
 import { ReactComponent as service } from "../../images/services.svg";
 import { ReactComponent as other } from "../../images/etc.svg";
 
+const book = "https://resourceimage.s3-us-west-2.amazonaws.com/books.svg"
 
 const componentMap = {
     'Non-Perishable Food': nonPerish,
