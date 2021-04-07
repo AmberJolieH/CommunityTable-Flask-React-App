cfd28d1543e49da48e6f437ff4c3cacb3bbf042f
Gabriel <gabriel.gutz@live.com>
2021-04-07 17:19:47 -0400

    added redux and backend route

--- react-app/src/components/Resources/claimedResources.js
+++ react-app/src/components/Resources/claimedResources.js
@@ -2,10 +2,20 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { jsx } from "@emotion/react";
-import { getPostedResources } from "../../store/resources"
+import { getClaimedResources } from "../../store/resources"
 import { Link, useParams } from "react-router-dom";
 
 const ClaimedResources = () => {
+    const userId = useSelector(state => state.session.user.id)
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getClaimedResources(userId))
+    }, [dispatch, userId])
+
+    // const resources = useSelector()
+
     return (
         <h1>This is the claimed Resources page</h1>
     )
