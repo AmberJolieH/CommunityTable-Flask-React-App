643fe6d6d68143b80f9c03c6b593f5ab41920a46
Daniel Upchurch <danielupchurch93@gmail.com>
2021-04-09 10:29:41 -0400

    began fixing login and sign up forms

--- react-app/src/components/login/LoginForm.js
+++ react-app/src/components/login/LoginForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux"
+import { useDispatch } from "react-redux";
 import { Redirect } from "react-router-dom";
 import * as sessionActions from "../../store/session";
 
@@ -7,7 +7,6 @@ const LoginForm = ({ authenticated, setAuthenticated }) => {
   const [errors, setErrors] = useState([]);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
 
   const dispatch = useDispatch();
 
@@ -23,13 +22,13 @@ const LoginForm = ({ authenticated, setAuthenticated }) => {
 
   const demoLogin = async (e) => {
     e.preventDefault();
-    const user = await dispatch(sessionActions.login('demo@aa.io', 'password'));
+    const user = await dispatch(sessionActions.login("demo@aa.io", "password"));
     if (!user.errors) {
       setAuthenticated(true);
     } else {
       setErrors(user.errors);
     }
-  }
+  };
 
   const updateEmail = (e) => {
     setEmail(e.target.value);
@@ -45,14 +44,15 @@ const LoginForm = ({ authenticated, setAuthenticated }) => {
 
   return (
     <>
-      <h1 className="cardheader" >Please Login</h1>
-      <div style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        marginTop: "2.5rem"
-      }}>
-
+      <h1 style={{textAlign: "center"}} className="cardheader">Please Login</h1>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginTop: "2.5rem",
+        }}
+      >
         <form onSubmit={onLogin} className="standard-card">
           <div>
             {errors.map((error, index) => (
@@ -78,19 +78,29 @@ const LoginForm = ({ authenticated, setAuthenticated }) => {
               value={password}
               onChange={updatePassword}
             />
-            <button style={{ marginLeft: "3.5rem", marginTop: "2rem" }} className="signupButton" type="submit">Login</button>
-
+            <button
+              style={{ marginLeft: "3.5rem", marginTop: "2rem" }}
+              className="signupButton"
+              type="submit"
+            >
+              Login
+            </button>
           </div>
-          <form onSubmit={demoLogin}>
-            <button style={{
+          <button
+            type="button"
+            onClick={demoLogin}
+            style={{
               textDecoration: "none",
               color: "black",
               padding: "0.5rem",
               margin: "1rem",
               backgroundColor: "rgb(237, 237, 237)",
-              marginLeft: "2.9rem"
-            }} className="text-button" type="submit">Demo User</button>
-          </form>
+              marginLeft: "2.9rem",
+            }}
+            className="text-button"
+          >
+            Demo User
+          </button>
         </form>
       </div>
     </>
